5a8c5dbe8a6eb7f9d5c6dae567e7f83a55baec99
jaky <weijunjie@onyx-international.com>
2017-09-09 10:15:21 +0800

    add the layout of tab1 and tab2.

--- app/src/main/java/com/onyx/test/styletest/fragment/Tab1Fragment.java
+++ app/src/main/java/com/onyx/test/styletest/fragment/Tab1Fragment.java
@@ -1,12 +1,21 @@
 package com.onyx.test.styletest.fragment;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.res.ResourcesCompat;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 import com.onyx.test.styletest.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by jaky on 2017/9/7 0007.
@@ -14,10 +23,48 @@ import com.onyx.test.styletest.R;
 
 public class Tab1Fragment extends BaseFragment {
 
+    @Bind(R.id.spinner_theme)
+    Spinner spinnerTheme;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab01, null);
+        ButterKnife.bind(this, view);
+        initSpinner();
         return view;
+    }
+
+    private void initSpinner() {
+        spinnerTheme.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                String[] systemTheme = getResources().getStringArray(R.array.system_theme);
+                switch (pos){
+                    case 0:
+                        setTheme();
+                        break;
+                    default:
+                        break;
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+
+    private void setTheme() {
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
     }
 }
--- app/src/main/java/com/onyx/test/styletest/fragment/Tab2Fragment.java
+++ app/src/main/java/com/onyx/test/styletest/fragment/Tab2Fragment.java
@@ -1,22 +1,99 @@
 package com.onyx.test.styletest.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.onyx.test.styletest.R;
+
+import org.apache.commons.lang3.StringUtils;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by jaky on 2017/9/7 0007.
  */
 
-public class Tab2Fragment extends BaseFragment {
+public class Tab2Fragment extends BaseFragment implements View.OnClickListener {
+    private static final int REC_REQUESTCODE = 1;
+    @Bind(R.id.src_file_path)
+    EditText srcFilePath;
+    @Bind(R.id.src_file_select)
+    Button srcFileSelect;
+    @Bind(R.id.dest_file_path)
+    EditText destFilePath;
+    @Bind(R.id.dest_file_select)
+    Button destFileSelect;
+    @Bind(R.id.start_replace)
+    Button startReplace;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab02, null);
+        ButterKnife.bind(this, view);
+        initView();
+        initEvent();
         return view;
+    }
+
+    private void initEvent() {
+        srcFileSelect.setOnClickListener(this);
+        destFileSelect.setOnClickListener(this);
+        startReplace.setOnClickListener(this);
+    }
+
+    private void initView() {
+        String src = srcFilePath.getText().toString().trim();
+        String dest = destFilePath.getText().toString().trim();
+        if (StringUtils.isNotBlank(src) && StringUtils.isNotBlank(dest)) {
+
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.src_file_select:
+                getSrcFile();
+                break;
+            case R.id.dest_file_select:
+                getDestFile();
+                break;
+            case R.id.start_replace:
+                startReplaceWork();
+                break;
+
+        }
+    }
+
+    private void startReplaceWork() {
+
+    }
+
+    public void getSrcFile() {
+        Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("text/plain");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        getActivity().startActivityForResult(intent,REC_REQUESTCODE);
+    }
+
+    public void getDestFile() {
+        Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("text/plain");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        getActivity().startActivityForResult(intent,REC_REQUESTCODE);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
     }
 }
