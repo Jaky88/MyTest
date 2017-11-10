e31b20b4dbac9068e042b62072d4360a06477334
jaky <weijunjie@onyx-international.com>
2017-11-10 19:07:13 +0800

    clean code.

--- app/src/main/java/com/onyx/test/mytest/view/fragment/Tab4Fragment.java
+++ app/src/main/java/com/onyx/test/mytest/view/fragment/Tab4Fragment.java
@@ -24,7 +24,6 @@ public class Tab4Fragment extends BaseFragment<FragmentTab4Binding> {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 bindingView.getBean().onWifiCheckedChanged(isChecked);
-                bindingView.invalidateAll();
             }
         });
     }
